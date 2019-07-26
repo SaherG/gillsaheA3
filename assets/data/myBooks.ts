467d1aed6f4035a83a85df612bbb2a4e65f3ed89
SaherG <52845587+SaherG@users.noreply.github.com>
2019-07-26 14:03:30 -0400

    Add files via upload

---
+++ assets/data/myBooks.ts
@@ -0,0 +1,9 @@
+import { bookInfo } from '../../app/bookinfo';
+
+export const MYBOOKS: bookInfo[] = 
+[
+    {bName: 'Don Quixote', bAuthor: 'Miguel de Cervantes', bGenre: 'Novel', bYear: '1605', bPicture: "assets/images/don.jpg"},
+    {bName: 'A Tale of Two Cities', bAuthor: 'Charles Dickens', bGenre: 'Historical novel', bYear: '1859', bPicture: "assets/images/tale.jpg"},
+    {bName: 'The Lord of the Rings', bAuthor: 'J. R. R. Tolkien', bGenre: 'Fantasy', bYear: '1954', bPicture: "assets/images/lord.jpg"},
+    {bName: 'The Little Prince', bAuthor: 'Antoine de Saint-Exupéry', bGenre: 'Fantasy', bYear: '1943', bPicture: "assets/images/prince.jpg"}
+]
